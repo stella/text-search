93211d7ceb534c222d359f0e8cf37346f78ce997
Jan Kubica <jankubica@protonmail.com>
2026-03-24 13:14:49 +0100

    chore: migrate from tsup to tsdown (#3)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
-  dts: true,
+  dts: { resolve: true },
   clean: true,
-  target: "es2022",
-  outDir: "dist",
+  sourcemap: true,
 });
